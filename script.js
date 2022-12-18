f50754fa6ef2d2d360f42ed6d5ba7dea1c30997e
imLines <a.code.room@gmail.com>
2022-12-18 22:33:00 +0100

    roadSection is finish for mobile support, add script for close the navbar when the user click on nav link and the image in the navbar was resize.

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ const toggle = document.querySelector('#nav-toggle');
 const buttonOfHamburger = document.querySelector('#button-hamburger');
 const menu = document.querySelector('header');
 const main = document.querySelector('main');
+const navLink = document.getElementsByClassName('nav-link');
+
+console.log(navLink)
 
 
-toggle.addEventListener('click', ()=>{
-    menu.className = toggle.checked ? 'block w-4/5 bg-black h-screen flex flex-col items-center fixed max-h-screen' : 'hidden';
+function closeOrOpenNavbar(){
+    menu.className = toggle.checked ? 'block w-4/5 bg-black h-screen flex flex-col items-center  fixed max-h-screen' : 'hidden';
     if(toggle.checked){
         buttonOfHamburger.src = '../images/icon/cross-white.png';
     }else{
         buttonOfHamburger.src = '../images/icon/menu-hamburger.png';
     }
-})
+}
 
+toggle.addEventListener('click', closeOrOpenNavbar)
 
+for (var i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', ()=>{
+        menu.className = 'hidden';
+        buttonOfHamburger.src = '../images/icon/menu-hamburger.png';
+        toggle.checked = false;
+    });
+  }
 
