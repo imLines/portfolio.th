83a4a422064aa8f35f084d51e585f7771ea32f79
imLines <a.code.room@gmail.com>
2023-01-05 14:32:45 +0100

    CSS update, Background home change and new button for download CV

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'home-background': "url('../images/background-hills.png')",
+        'home-background': "url('../images/background-home.webp')",
         'wwiDEV-background': "url('../images/projects/wwiDEV/wwiDEV-background.png')",
         'pyjamzz-background': "url('../images/projects/pyjamzz/BackgroundPY.png')"
       },
